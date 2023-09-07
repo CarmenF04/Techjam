4173d8c6d1fe2a35fbd6feb2dc9655de2a7eeab2
Carmen <33059@ma-web.nl>
2023-09-07 12:00:45 +0200

    for loop added and if statements

--- index.php
+++ index.php
@@ -9,7 +9,29 @@
 </head>
 <body>
 <?php
+for ($i = 33; $i <= 543; $i++) {
+    $output = '';
 
+    if ($i % 2 == 0) {
+        $output .= '<span class="bold">';
+    }
 
+    if ($i % 5 == 0 && $i % 9 == 0) {
+        $output .= 'Web';
+    } elseif ($i % 5 == 0) {
+        $output .= 'Software';
+    } elseif ($i % 9 == 0) {
+        $output .= 'Developer';
+    } else {
+        $output .= $i;
+    }
+
+    if ($i % 2 == 0) {
+        $output .= '</span>';
+    }
+
+    echo $output . '<br>';
+}
 ?>
+</body>
 </html>
